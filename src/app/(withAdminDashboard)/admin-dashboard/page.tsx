9cd6722d77060ccdf628ecfd84d2a6fe51ad9ac9
Tahsin0909 <tajisan789@gmail.com>
2024-11-19 17:24:22 +0600

    Added verificatin page tab

--- src/app/(withAdminDashboard)/admin-dashboard/page.tsx
+++ src/app/(withAdminDashboard)/admin-dashboard/page.tsx
@@ -1,12 +1,35 @@
-import NotVerified from '@/components/Dashboard/components/notVerified/NotVerified';
+import General from '@/components/Dashboard/components/settings/General';
+import ManagePlans from '@/components/Dashboard/components/settings/MangePlans';
+import TabPage from '@/components/shared/tabPage/TabPage';
+import { SubscriptionsPlan } from '@/lib/fakeData/subscriptionPlans';
+import { user } from '@/lib/fakeData/user';
 import React from 'react';
+import { FaClipboardList, FaUserCog } from 'react-icons/fa';
 
 const page = () => {
+
+    const tabs = [
+        {
+            label: "General",
+            value: "general",
+            icon: <FaUserCog className="min-w-6 min-h-6 mr-2" />,
+            component: <General user={user} />
+        },
+        {
+            label: "Manage Plans",
+            value: "manage-plans",
+            icon: <FaClipboardList className="min-w-6 min-h-6 mr-2" />,
+            component: <ManagePlans subscriptionsPlan={SubscriptionsPlan} plans={user.plans} />
+        }
+    ];
+
+    // demo link for breadcrumb 
+    // http://localhost:3000/admin-dashboard/settings?tab=general/subtab1/subtab2
+
+    
     return (
-        <div className='dashboard-containers '>
-            <NotVerified />
-        </div>
-    );
+        <TabPage defaultTab="general" tabs={tabs} />
+    )
 };
 
 export default page;
