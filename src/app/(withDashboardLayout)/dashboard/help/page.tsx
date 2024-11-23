c234acd5ab5df5c52e123814a90d09d81c95498b
Tahsin0909 <tajisan789@gmail.com>
2024-11-23 11:33:42 +0600

    Completd Help page

--- src/app/(withDashboardLayout)/dashboard/help/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/help/page.tsx
@@ -1,10 +1,12 @@
-import UnderCOnstruction from '@/components/underConstruction/UnderConstruction';
+import HelpPages from '@/components/Dashboard/pages/help/HelpPages';
+import BreadCrumb from '@/components/Shared/breadCrumb/BreadCrumb';
 import React from 'react';
 
 const page = () => {
     return (
-        <div>
-           <UnderCOnstruction pageName='Helps'/>
+        <div className='dashboard-containers'>
+            <BreadCrumb />
+            <HelpPages />
         </div>
     );
 };
---
+++ src/components/Dashboard/pages/help/HelpPages.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import GradCircleWrapper from '@/components/Shared/grad-circleWrapper/GradCircleWrapper';
+import { Button } from '@nextui-org/react';
+import React from 'react';
+import { RiCustomerService2Line } from 'react-icons/ri';
+
+const HelpPages = () => {
+    return (
+        <div>
+            <p className='mt-4 py-5 border-b'>Email: <span>example@gmail.com</span></p>
+            <GradCircleWrapper>
+                <div className='flex flex-col items-center justify-center lg:space-y-6 md:space-y-4 space-y-2 aspect-square'>
+                    <p className='text-2xl font-medium text-center'>Let’s work together and solve the problem!</p>
+                    <p className='max-w-sm text-center'>Our customer care will guide you step by step to your solution</p>
+                    <Button radius='sm' className='bg-primary text-white flex items-center gap-2'>
+                        Start Chat
+                        <RiCustomerService2Line className='min-h-6 min-w-6' />
+                    </Button>
+                </div>
+            </GradCircleWrapper>
+        </div>
+    );
+};
+
+export default HelpPages;
