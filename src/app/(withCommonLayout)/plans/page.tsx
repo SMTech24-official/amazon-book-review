da69eb76849335fc24ece7b479e04fade89a0e1a
Tahsin0909 <tajisan789@gmail.com>
2024-12-11 10:09:05 +0600

    working on plans

--- src/app/(withCommonLayout)/plans/page.tsx
+++ src/app/(withCommonLayout)/plans/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 const PlansPage = () => {
     return (
         <>
-            <PlansComponent/>
+            <PlansComponent />
         </>
     );
 };
--- src/components/Dashboard/components/settings/MangePlans.tsx
+++ src/components/Dashboard/components/settings/MangePlans.tsx
@@ -3,13 +3,17 @@ import { SubscriptionPlan } from "@/lib/types/type";
 import SubscriptionCards from "../cards/subscriptionCards/SubscriptionCards";
 
 export default function ManagePlans({ subscriptionsPlan }: { plans: string, subscriptionsPlan: SubscriptionPlan[] }) {
+
+
+
     return (
         <div className="space-y-6">
             <div className="bg-white px-6 pb-6 rounded-lg shadow-sm">
                 <h2 className="text-lg font-semibold mb-6 border-b pb-2">Manage Plans</h2>
                 <div className="grid xl:grid-cols-2 gap-6">
-                    {subscriptionsPlan?.map((plan, index) => ( <SubscriptionCards key={index}  plan={plan} /> ))}
+                    {subscriptionsPlan?.map((plan, index) => (<SubscriptionCards key={index} plan={plan} />))}
                 </div>
+
                 <div className="flex items-center justify-center gap-4 mt-6">
                     <Button variant="ghost" className="border">
                         Discard
