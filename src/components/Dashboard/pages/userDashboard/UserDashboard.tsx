85fdf003fa74f4787e81bf96c90f1105fb023a74
Tahsin0909 <tajisan789@gmail.com>
2024-12-12 11:05:42 +0600

    Completed not verifided func

--- src/components/Dashboard/pages/userDashboard/UserDashboard.tsx
+++ src/components/Dashboard/pages/userDashboard/UserDashboard.tsx
@@ -1,9 +1,13 @@
+"use client"
 import TabPage from '@/components/common/tabPage/TabPage';
 import Activity from '../../components/userDashBoard/Activity';
 import ReviewHistory from '../../components/userDashBoard/ReviewHistory';
 import UDBookReview from '../../components/userDashBoard/UDBookReview';
+import { useUserDataQuery } from '@/redux/features/auth/authApi';
+import NotVerified from '../../components/notVerified/NotVerified';
 
 const UserDashboard = () => {
+    const { data: UserData } = useUserDataQuery(undefined)
 
     const tabs = [
         {
@@ -25,18 +29,25 @@ const UserDashboard = () => {
 
     return (
         <div>
-            <div className='hidden sm:grid grid-cols-5 items-start justify-center'>
-                <div className='col-span-3 mr-10 '>
-                    <UDBookReview />
+            {
+                (UserData?.data.profileImage || UserData?.data.amazonAuthorPageLink) ? <div>
+                    <div className='hidden sm:grid grid-cols-5 items-start justify-center'>
+                        <div className='col-span-3 mr-10 '>
+                            <UDBookReview />
+                        </div>
+                        <div className='grid grid-cols-1 col-span-2 '>
+                            <ReviewHistory />
+                            <Activity />
+                        </div>
+                    </div>
+                    <div className='sm:hidden block'>
+                        <TabPage defaultTab="books" tabs={tabs} />
+                    </div>
+                </div> : <div className='min-h-[70vh] flex items-center justify-center'>
+                    <NotVerified />
                 </div>
-                <div className='grid grid-cols-1 col-span-2 '>
-                    <ReviewHistory />
-                    <Activity />
-                </div>
-            </div>
-            <div className='sm:hidden block'>
-                <TabPage defaultTab="books" tabs={tabs} />
-            </div>
+            }
+
         </div>
     );
 };
